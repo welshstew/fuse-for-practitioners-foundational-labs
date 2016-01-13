c853ef677fd6a82ad1d5d16abd04508792d301ed
Charles Moulliard <ch007m@gmail.com>
2016-01-13 12:45:36 +0100

    Clean code, test, properties files for the usecase1

--- usecase-1/routing/src/test/java/org/fuse/usecase/ValidateTransformationTest.java
+++ usecase-1/routing/src/test/java/org/fuse/usecase/ValidateTransformationTest.java
@@ -20,37 +20,31 @@ import java.io.IOException;
 
 public class ValidateTransformationTest extends CamelSpringTestSupport {
 
-    @EndpointInject(uri = "mock:csv2json-test-output") private MockEndpoint resultEndpoint;
 
-    @Produce(uri = "direct:csv2json-test-input") private ProducerTemplate startEndpoint;
-
-    @Test public void transform() throws Exception {
+    @Test
+    public void transform() throws Exception {
         // setup expectations
-        resultEndpoint.expectedMessageCount(1);
+
         // set expected body as the unpretty print version of the json
         // (flattened)
-        resultEndpoint.expectedBodiesReceived(jsonUnprettyPrint(readFile("src/test/data/account.json")));
 
         // run test
-        startEndpoint.sendBody(readFile("src/test/data/customer.csv"));
+
 
         // verify results
-        resultEndpoint.assertIsSatisfied();
+
     }
 
-    @Override protected RouteBuilder createRouteBuilder() throws Exception {
+    @Override
+    protected RouteBuilder createRouteBuilder() throws Exception {
         return new RouteBuilder() {
             public void configure() throws Exception {
-                from("direct:csv2json-test-input")
-                   .log("Before transformation:\n ${body}")
-                   .to("ref:csv2json")
-                   .log("After transformation:\n ${body}")
-                   .to("mock:csv2json-test-output");
             }
         };
     }
 
-    @Override protected AbstractXmlApplicationContext createApplicationContext() {
+    @Override
+    protected AbstractXmlApplicationContext createApplicationContext() {
         return new ClassPathXmlApplicationContext("META-INF/spring/camel-context.xml");
     }
 
